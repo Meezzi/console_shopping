f66ecb464e76afacc46ba1f6170560e3a47634d7
Meezzi <alswl-3016@naver.com>
2025-03-14 03:45:24 +0900

    fix: 객체 생성을 while문 바깥에서 생성
    while문이 돌 때마다 객체를 초기화하지 않기 위함

--- bin/main.dart
+++ bin/main.dart
@@ -2,14 +2,14 @@ import 'dart:io';
 import '../bin/domain/shopping_mall.dart';
 
 void main() {
+  var emart = ShoppingMall();
+
   while (true) {
     stdout.write(
       '[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료\n',
     );
 
     String? input = stdin.readLineSync();
-
-    var emart = ShoppingMall();
 
     switch (input) {
       case '1':
